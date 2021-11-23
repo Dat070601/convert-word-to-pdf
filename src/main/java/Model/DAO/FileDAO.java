8dc1408d99eb11e42ff21287c84273db368de14d
ShikamaruBH <huynhphuquy712001asus4.4.2@gmail.com>
2021-11-23 14:17:32 +0700

    Add connection in FileDAO

--- src/main/java/Model/DAO/FileDAO.java
+++ src/main/java/Model/DAO/FileDAO.java
@@ -1,6 +1,20 @@
 package Model.DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class FileDAO {
+	Connection con;
+	public FileDAO() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DULIEU", "root", "");
+		} catch (Exception e) {}
+	}
 	public static byte[] DowloadFile() {
 	
 		return null;
