81d961b3b560946f0638c4f6ab359418b43ed473
Kim-bk <68554677+Kim-bk@users.noreply.github.com>
2021-11-25 08:27:41 +0700

    feat/25,27,28-download_file
    * kim - reup DowloadFile Modules
    
    * change location file appose .jar
    
    * kim - uncomment
    
    * kim - up code remove watermark
    
    * kim - remove unnecessary code
    
    * kim - delete folder pdf_files

---
+++ src/main/java/Controller/DowloadFileServlet.java
@@ -0,0 +1,83 @@
+package Controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Model.BEAN.File;
+import Model.BO.FileBO;
+
+/**
+ * Servlet implementation class DowloadFileServlet
+ */
+@WebServlet("/DowloadFileServlet")
+public class DowloadFileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DowloadFileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	       try {  
+	           int id = 0;
+	           try {
+	               id = Integer.parseInt(request.getParameter("id"));
+	           } catch (Exception e) {
+
+	           }
+	           File file = new FileBO().GetFilePDFFromDB(id);
+	           
+	           if (file == null) {
+	               response.getWriter().write("Không tìm thấy data!");
+	               return;
+	           }
+	           
+	           String fileName = file.getFileName();
+	           //System.out.println("File Name: " + fileName);
+	           String contentType = this.getServletContext().getMimeType(fileName);
+	           //System.out.println("Content Type: " + contentType);
+	           response.setHeader("Content-Type", contentType);
+	           response.setHeader("Content-Length", String.valueOf(file.getData().length()));
+	           response.setHeader("Content-Disposition", "inline; filename=\"" + file.getFileName() + "\"");
+	           Blob fileData = file.getData();
+	           InputStream is = fileData.getBinaryStream();
+	           byte[] bytes = new byte[1024];
+	           int bytesRead;
+	           while ((bytesRead = is.read(bytes)) != -1) {
+	               response.getOutputStream().write(bytes, 0, bytesRead);
+	           }
+	           is.close();
+
+	       } catch (Exception e) {
+	           throw new ServletException(e);
+	       } 
+	       finally
+	       {
+	    	   new FileBO().CloseConQuietly();
+	       }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	} 
+}
--- src/main/java/Model/BO/FileBO.java
+++ src/main/java/Model/BO/FileBO.java
@@ -2,29 +2,37 @@ package Model.BO;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import com.aspose.words.*;
 
+import Model.BEAN.File;
 import Model.DAO.FileDAO;
 
 public class FileBO {
-
 	public byte[] ConvertFileToPDF(byte[] docFile) throws Exception
 	{
 		ByteArrayInputStream inStream = new ByteArrayInputStream(docFile);
-		
 		// Load Document from inStream
 		Document doc = new Document(inStream);
-		
+		// remove watermark
+		if (doc.getWatermark().getType() == WatermarkType.TEXT) {
+			doc.getWatermark().remove();
+		}
 		// Save the modified document into out stream
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		doc.save(baos, SaveFormat.PDF);
 		return baos.toByteArray();
 	}
 	
-	public byte[] DownloadFile()
+	public File GetFilePDFFromDB(int id) throws SQLException
 	{
-		return FileDAO.DowloadFile();
+		return new FileDAO().GetFilePDFFromDB(id);
+	}
+	
+	public void CloseConQuietly()
+	{
+		new FileDAO().CloseConQuietly();
 	}
 }
