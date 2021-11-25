4712511fcc1e57e769bc0d029f98fdb480b92e72
ShikamaruBH <74344219+ShikamaruBH@users.noreply.github.com>
2021-11-25 10:30:33 +0700

    feat(18, 24, 26, 31): upload file
    Chức năng upload file

--- src/main/java/Model/BO/FileBO.java
+++ src/main/java/Model/BO/FileBO.java
@@ -2,7 +2,6 @@ package Model.BO;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import com.aspose.words.*;
@@ -11,6 +10,21 @@ import Model.BEAN.File;
 import Model.DAO.FileDAO;
 
 public class FileBO {
+	public boolean UploadFile(byte[] data, File file) {
+		if (file != null) {
+			File newFile = new FileDAO().UploadFile(file);
+			new XuLy(data, newFile).start();
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean UpdateFile(File file) {
+		if (file == null)
+			return false;
+		return new FileDAO().UpdateFile(file);
+	}
+	
 	public byte[] ConvertFileToPDF(byte[] docFile) throws Exception
 	{
 		ByteArrayInputStream inStream = new ByteArrayInputStream(docFile);
@@ -34,5 +48,32 @@ public class FileBO {
 	public void CloseConQuietly()
 	{
 		new FileDAO().CloseConQuietly();
+	}
+}
+
+class XuLy extends Thread {
+	String stringEnd = " - DocToPdf - LTM";
+	byte[] data;
+	File file;
+	public XuLy(byte[] data, File file) {
+		this.data = data;
+		this.file = file;
+	}
+	public void run() {
+		if (this.file != null) {
+			try {	
+				System.out.println("Converting " + this.file.getFileName() + " ...");
+				// Convert file
+				this.data = new FileBO().ConvertFileToPDF(this.data);
+				// Update data
+				this.file.setData(new javax.sql.rowset.serial.SerialBlob(this.data));	
+				this.file.setFileName(this.file.getFileName() + stringEnd);
+				this.file.setStatus(true);
+				new FileBO().UpdateFile(this.file);
+			} catch (Exception e) {
+				System.out.println("Convert or set blob error: " + e.toString());
+			}
+			System.out.println("Convert " + this.file.getFileName() + " done !");
+		}
 	}
 }
